13e104e1afe5c9c3784a7d8603a53bf3641ae2f5
Nick <plus0bit@gmail.com>
2018-11-17 23:33:32 +0300

    Add contacts controller and new.html.erb view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resource :contacts, only: [:new, :create]
   resources :articles
+
   
 end
