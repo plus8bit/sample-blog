c59acc654239a35bb59792a59866068b6dc0ad74
Nick <plus0bit@gmail.com>
2018-11-21 22:15:48 +0300

    Add comments to post

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about'
 
   resource :contacts, only: [:new, :create], path_names: { :new => ''} 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   
 end
